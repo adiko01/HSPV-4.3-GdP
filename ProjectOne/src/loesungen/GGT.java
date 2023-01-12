2dc405626f82cd35e380dd9ab04237e0ac37118b
Adrian Ivo Kolar <dev@adiko01.de>
2023-01-12 14:22:45 +0100

    Lösung GGT (Folie 19) hinzugefügt

---
+++ ProjectOne/src/loesungen/GGT.java
@@ -0,0 +1,27 @@
+package loesungen;
+
+public class GGT {
+
+	
+	
+	public static void main(String[] args)
+	{
+		
+		int a = 20;
+		int b = 8;
+		
+		if (a<=0||b<=0)
+			a=b=0;
+			
+		while (a!=b) {
+			if (a<b)
+				b=b-a;
+			else
+				a=a-b;
+		}
+		
+
+		System.out.println(a);
+		
+	}
+}
