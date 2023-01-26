eae1786b56a90841983db22f7c72f2fe74a26dc6
Adrian Ivo Kolar <dev@adiko01.de>
2023-01-26 11:55:43 +0100

    Kleine Änderrung

--- Uebung_2_5/src/Aufgabe_5.java
+++ Uebung_2_5/src/Aufgabe_5.java
@@ -5,15 +5,12 @@ public class Aufgabe_5 {
 		//Das angelegte Kapital als Double
 		System.out.print("Das anzulegende Kapital: ");
 		double kap = new java.util.Scanner(System.in).nextDouble();
-		System.out.print("\n");
 		//Den Zinssatz als Double
 		System.out.print("Der Zinssatz: ");
 		double zins = new java.util.Scanner(System.in).nextDouble();
-		System.out.print("\n");
 		//Die Anzahl der Jahre bzw. der Zinsausschüttungen
-		System.out.print("Die zeit in der das Kapital angelegt werden soll: ");
+		System.out.print("Die Zeit in der das Kapital angelegt werden soll: ");
 		int time = new java.util.Scanner(System.in).nextInt();
-		System.out.print("\n");
 		
 		kap = kap + kap * (zins/100) * time;
 		System.out.println("Nach " + time + " Jahren wird ein Kapital von " + kap +"€");
