f8d33fcbf7369f4c7a7e35d655cc0c2814ad423f
Adrian Ivo Kolar <dev@adiko01.de>
2023-02-08 13:58:38 +0100

    Nachtrag Vorlesung 2023-02-02

---
+++ 4-3-1-Wambach/src/beispiele/Kontrollstrukturen/Aufgabe_5.java
@@ -0,0 +1,35 @@
+package beispiele.Kontrollstrukturen;
+
+
+public class Aufgabe_5 {
+
+	public static void main(String[] args) {
+			
+		double kapital = 0.0;
+		double zinssatz = 0.0;
+		int laufzeit = 0;
+		
+		System.out.print("Welchen Geldbetrag wollen Sie anlegen: ");
+		kapital = new java.util.Scanner(System.in).nextDouble();
+		
+		System.out.print("Welchen Zinssatz bekommen Sie: ");
+		zinssatz = new java.util.Scanner(System.in).nextDouble();
+		
+		System.out.print("Wie viele Jahre wollen Sie auflisten: ");
+		laufzeit = new java.util.Scanner(System.in).nextInt();
+
+		
+		for (int i=1; i <= laufzeit; i++)
+		{
+			kapital = kapital + kapital * zinssatz / 100;
+			System.out.println("Nach " + i + " Jahren haben Sie: " + kapital + "€");
+			
+		}
+		
+		//System.out.println(((long)(kapital*100))/100.0);
+		
+		
+		
+	}
+
+}
---
+++ 4-3-1-Wambach/src/beispiele/Kontrollstrukturen/Aufgabe_6.java
@@ -0,0 +1,64 @@
+package beispiele.Kontrollstrukturen;
+
+
+public class Aufgabe_6 {
+	public static void main (String[] args)
+	{
+		System.out.println("Herzlich Willkommen beim Zahlenraten!");
+		System.out.println("");
+		
+		boolean playAgain = false;
+		
+		
+		
+		do
+		{
+			int randomNumber = (int) (Math.random() * 100 + 1);
+			int trials = 0;
+			boolean won = false;
+			
+			do
+			{
+				trials++;
+				System.out.print(trials + ". Versuch: ");
+				int guess = new java.util.Scanner(System.in).nextInt();
+				
+				
+				if (guess < randomNumber)
+				{
+					System.out.println("Die gesuchte Zahl ist größer.");
+				}
+				else if (guess > randomNumber)
+				{
+					System.out.println("Die gesuchte Zahl ist kleiner.");
+				}
+				else
+				{
+					won = true;
+					System.out.println("Richtig!");
+					System.out.println("Sie haben " + trials + " Versuche gebraucht.");
+				}
+			} while ( !won && trials < 10);
+			
+			if (!won)
+			{
+				System.out.println("Sie haben leider keine Versuche mehr");
+				System.out.println("Die gesuchte Zahl war die " + randomNumber);
+			}
+			
+			System.out.println("Möchten Sie noch einmal spielen? (y/n)");
+			playAgain = switch(new java.util.Scanner(System.in).nextLine().toLowerCase().charAt(0))
+					{
+						case 'y' -> true;
+						default -> false;
+					};
+			
+			
+			
+		} while (playAgain);
+		
+		System.out.println("Auf Wiedersehen!");
+		
+	}
+
+}
