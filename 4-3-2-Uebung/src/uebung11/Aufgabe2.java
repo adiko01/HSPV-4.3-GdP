bfba8ed9cd2e99557646ba9487eb41e8ff4764c6
Adrian Ivo Kolar <dev@adiko01.de>
2023-04-13 15:08:54 +0200

    Übung 11 - Aufgabe 2

---
+++ 4-3-2-Uebung/src/uebung11/Aufgabe2.java
@@ -0,0 +1,63 @@
+package uebung11;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Aufgabe2 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Aufgabe2 frame = new Aufgabe2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Aufgabe2() {
+		setTitle("Übung 11 - Aufgabe 2");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 400, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel.setBackground(Color.BLUE);
+				panel.setBounds(35, 44, 100, 100);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel.setBackground(Color.RED);
+				panel.setBounds(35, 44, 50, 50);
+			}
+		});
+		panel.setBackground(Color.RED);
+		panel.setBounds(35, 44, 50, 50);
+		contentPane.add(panel);
+	}
+
+}
