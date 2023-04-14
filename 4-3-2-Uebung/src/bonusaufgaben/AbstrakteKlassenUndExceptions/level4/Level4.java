5ae9a5fbf256503cf08a8091b7682f6b77e2d0a2
Adrian Ivo Kolar <dev@adiko01.de>
2023-04-14 12:14:09 +0200

    Bonusaufgaben - Abstrakte Klassen und Exceptions - Level 4 und 5

---
+++ 4-3-2-Uebung/src/bonusaufgaben/AbstrakteKlassenUndExceptions/level4/Level4.java
@@ -0,0 +1,27 @@
+package bonusaufgaben.AbstrakteKlassenUndExceptions.level4;
+
+public class Level4 {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(factorial(5));
+			System.out.println(factorial(0));
+			System.out.println(factorial(-5));
+		} catch (NegativeNumberException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	private static int factorial(int n) throws NegativeNumberException {
+		int fac = n;
+		if (n < 0 ) {
+			throw new NegativeNumberException();
+		} else {
+			for (int i = 1 ; i < n ; i++) {
+				fac = fac * i;
+			}
+		}
+		return fac;
+	}
+}
---
+++ 4-3-2-Uebung/src/bonusaufgaben/AbstrakteKlassenUndExceptions/level4/NegativeNumberException.java
@@ -0,0 +1,10 @@
+package bonusaufgaben.AbstrakteKlassenUndExceptions.level4;
+
+public class NegativeNumberException extends Exception {
+	NegativeNumberException() {
+		super();
+	}
+	NegativeNumberException(String msg) {
+		super(msg);
+	}
+}
